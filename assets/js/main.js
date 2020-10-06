1d16fdaad58a857c08d7e2f7f88591ec1d096517
WayupKG <adikgk232323@gmail.com>
2020-10-06 23:20:09 +0600

    learning 1

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,24 @@
 "use strict";
  
 
-const user = "Adi";
+// Урок 1
 
-alert(`Hello ${user}`);
+const nubmerOfFilms = +prompt('Сколько фильмов вы посмотрели?', '1, 2, 3');
+
+const personsMovieDB = {
+    count: nubmerOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt('Один из просмотренных фильмов?', ''),
+      b = prompt('На сколько оцените его?', ''),
+      c = prompt('Один из просмотренных фильмов?', ''),
+      d = prompt('На сколько оцените его?', '');
+    
+personsMovieDB.movies[a] = b;
+personsMovieDB.movies[c] = d;
+
+console.log(personsMovieDB);
